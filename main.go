b1c68f71208dd8bc5f6ecbeea6911cbc912285c8
Andrea Cedraro <a.cedraro@gmail.com>
2014-12-06 11:52:49 +0100

    Rewrite of main and picker

--- main.go
+++ main.go
@@ -3,82 +3,222 @@ package main
 import (
 	"bufio"
 	"flag"
-	"fmt"
-	"io"
 	"os"
 	"runtime"
 	"unicode/utf8"
 
-	"github.com/vheon/picker/terminal"
-	"github.com/vheon/picker/tty"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
-func appendChar(s string, b rune) string {
-	return s + string(b)
+const VisibleCandidates int = 20
+
+const (
+	keyCtrlC     = 3
+	keyCtrlD     = 4
+	keyCtrlU     = 21
+	keyCtrlW     = 23
+	keyEnter     = '\r'
+	keyEscape    = 27
+	keyBackspace = 127
+	// keyUp
+	// keyDown
+	// keyLeft
+	// keyRight
+	// keyHome
+	// keyEnd
+)
+
+type movements struct {
+	Up    int
+	Down  int
+	Left  int
+	Right int
 }
 
-func backspace(s string) string {
-	if l := len(s); l > 0 {
-		_, size := utf8.DecodeLastRuneInString(s)
-		return s[:l-size]
+func move(directions movements) string {
+	count := directions.Down + directions.Up + directions.Left + directions.Right
+	movement := make([]rune, 3*(count))
+	m := movement
+	for i := 0; i < directions.Up; i++ {
+		m[0] = keyEscape
+		m[1] = '['
+		m[2] = 'A'
+		m = m[3:]
 	}
-	return s
-}
-
-func readAllCandidates(r io.Reader) []Candidate {
-	scanner := bufio.NewScanner(r)
-	var lines []Candidate
-	for scanner.Scan() {
-		lines = append(lines, NewCandidate(scanner.Text()))
+	for i := 0; i < directions.Down; i++ {
+		m[0] = keyEscape
+		m[1] = '['
+		m[2] = 'B'
+		m = m[3:]
 	}
-	return lines
-}
-
-func handle_input(picker *Picker, view *View, key rune) *View {
-	switch key {
-	case terminal.Ctrl_N:
-		view.Down()
-	case terminal.Ctrl_P:
-		view.Up()
-	case terminal.Backspace:
-		view = picker.Answer(backspace(view.Query))
-	case terminal.LF:
-		view.Done = true
-		view.ClearPrompt()
-	case terminal.Ctrl_U, terminal.Ctrl_W:
-		view = picker.Answer("")
-	default:
-		view = picker.Answer(appendChar(view.Query, key))
+	for i := 0; i < directions.Left; i++ {
+		m[0] = keyEscape
+		m[1] = '['
+		m[2] = 'D'
+		m = m[3:]
 	}
-	return view
+	for i := 0; i < directions.Right; i++ {
+		m[0] = keyEscape
+		m[1] = '['
+		m[2] = 'C'
+		m = m[3:]
+	}
+
+	return string(movement)
 }
 
-var visibleRows = flag.Int("h", 20, "Number of visible candidates")
+var (
+	SaveCursorPosition    string = string([]rune{keyEscape, '[', 's'})
+	RestoreCursorPosition string = string([]rune{keyEscape, '[', 'u'})
+	EraseDisplay          string = string([]rune{keyEscape, '[', 'J'})
+
+	ReverseColor string = string([]rune{keyEscape, '[', '7', 'm'})
+	ResetColor   string = string([]rune{keyEscape, '[', '0', 'm'})
+)
+
+func OpenTTY() (*os.File, error) {
+	return os.OpenFile("/dev/tty", os.O_RDWR, 0)
+}
+
+func MakeRaw(tty *os.File) (*terminal.State, error) {
+	return terminal.MakeRaw(int(tty.Fd()))
+}
+
+func Restore(tty *os.File, state *terminal.State) error {
+	return terminal.Restore(int(tty.Fd()), state)
+}
+
+func GetSize(tty *os.File) (int, int, error) {
+	return terminal.GetSize(int(tty.Fd()))
+}
+
+func TTYReverse(str string) string {
+	return ReverseColor + str + ResetColor
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	flag.Parse()
 
-	tty := tty.New()
-	tty.Stty("-echo", "-icanon")
-	defer tty.Restore()
-
-	term := terminal.NewTerminal(tty)
-
-	if *visibleRows > term.Height {
-		*visibleRows = term.Height - 1
+	tty, err := OpenTTY()
+	if err != nil {
+		panic(err)
 	}
-	if *visibleRows < 1 {
-		*visibleRows = 1
+	width, height, err := GetSize(tty)
+	if err != nil {
+		panic(err)
 	}
+	originalState, err := MakeRaw(tty)
+	if err != nil {
+		panic(err)
+	}
+	defer Restore(tty, originalState)
 
-	picker := NewPicker(readAllCandidates(os.Stdin), *visibleRows)
-	view := picker.Answer("")
-	term.MakeRoom(view.Height)
-	view.DrawOnTerminal(term)
-	for !view.Done {
-		view = handle_input(picker, view, tty.ReadRune())
-		view.DrawOnTerminal(term)
+	visible := min(VisibleCandidates, height)
+	prompt := "> "
+	picker := NewPicker(prompt, visible, width, os.Stdin)
+
+	in := make(chan rune)
+	quit := make(chan struct{})
+	end := make(chan struct{})
+	back := make(chan struct{})
+	clear := make(chan struct{})
+	go func() {
+		reader := bufio.NewReader(tty)
+		for {
+			r, _, err := reader.ReadRune()
+			if err != nil {
+				break
+			}
+			switch r {
+			case keyEscape, keyCtrlC:
+				quit <- struct{}{}
+			case keyEnter:
+				end <- struct{}{}
+			case keyBackspace:
+				back <- struct{}{}
+			case keyCtrlU, keyCtrlW:
+				clear <- struct{}{}
+			default:
+				in <- r
+			}
+		}
+		close(quit)
+		close(end)
+		close(in)
+	}()
+
+	tty.WriteString(picker.String())
+
+	// go to the start of the first line
+	tty.WriteString(move(movements{
+		Up:    VisibleCandidates,
+		Down:  0,
+		Left:  len(picker.view.lines[VisibleCandidates-1]),
+		Right: 0,
+	}))
+
+	// save the pos
+	tty.WriteString(SaveCursorPosition)
+
+	// move the cursor to the right prompt position
+	tty.WriteString(move(movements{
+		Up:    0,
+		Down:  0,
+		Left:  0,
+		Right: len(picker.prompt),
+	}))
+
+	for {
+		select {
+		case r := <-in:
+			picker.query += string(r)
+		case <-back:
+			_, size := utf8.DecodeLastRuneInString(picker.query)
+			picker.query = picker.query[:len(picker.query)-size]
+			picker.valid = len(picker.all)
+		case <-clear:
+			picker.query = ""
+			picker.valid = len(picker.all)
+		case <-quit:
+			os.Exit(1)
+		case <-end:
+			tty.WriteString(move(movements{
+				Up:    0,
+				Down:  0,
+				Right: 0,
+				Left:  len(picker.prompt) + len(picker.query),
+			}))
+			tty.WriteString(picker.view.Selected() + "\n")
+			return
+		}
+
+		// do the reorder
+		picker.Sort()
+
+		// go to the stored position
+		tty.WriteString(RestoreCursorPosition)
+
+		// clear the screen
+		tty.WriteString(EraseDisplay)
+
+		// write what we should see
+		tty.WriteString(picker.String())
+
+		// move the cursor to the right prompt position
+		tty.WriteString(RestoreCursorPosition)
+		tty.WriteString(move(movements{
+			Up:    0,
+			Down:  0,
+			Left:  0,
+			Right: len(picker.prompt) + len(picker.query),
+		}))
 	}
-	fmt.Println(view.Selected())
 }
--- picker.go
+++ picker.go
@@ -1,97 +1,177 @@
 package main
 
 import (
+	"bufio"
+	"io"
 	"sort"
 	"sync"
-
-	"github.com/vheon/picker/terminal"
 )
 
-type View struct {
-	Height int
-	Query  string
-	Rows   []string
-	Done   bool
-
-	index  int
-	prompt string
-}
-
-func (v *View) Index() int {
-	return v.index
-}
-
-func (v *View) Selected() string {
-	if len(v.Rows)-1 < v.index {
-		return ""
-	}
-	return v.Rows[v.index]
-}
-
-func (v *View) Down() {
-	if v.index < len(v.Rows)-1 {
-		v.index++
-	}
-}
-
-func (v *View) Up() {
-	if v.index > 0 {
-		v.index--
-	}
-}
-
-func (v *View) ClearPrompt() {
-	v.Query = ""
-	v.prompt = ""
-}
-
-func (v *View) DrawOnTerminal(t *terminal.Terminal) {
-	t.HideCursor()
-	defer t.ShowCursor()
-
-	start_row := t.Height - v.Height - 1
-
-	for i, row := range v.toAnsiForm(t) {
-		t.WriteToLine(start_row+i, row)
-	}
-
-	t.MoveTo(start_row, len(v.Query)+len(v.prompt))
-}
-
-func (v *View) toAnsiForm(t *terminal.Terminal) []string {
-	rows := make([]string, v.Height+1)
-	rows[0] = v.prompt + v.Query
-	for i, row := range v.Rows {
-		rows[i+1] = cutAt(row, t.Width)
-	}
-	if len(v.Rows) > 0 {
-		rows[v.Index()+1] = terminal.AnsiInverted(v.Selected())
-	}
-	return rows
-}
-
-func cutAt(s string, width int) string {
-	if len(s) > width {
-		return s[:width]
-	}
-	return s
-}
-
 type Picker struct {
-	all     []Candidate
-	visible int
-	blank   *View
+	all    []Candidate
+	valid  int
+	prompt string
+	query  string
+	view   View
+	blank  View
 }
 
-func NewPicker(candidates []Candidate, visible int) *Picker {
-	picker := &Picker{
-		all:     candidates,
-		visible: visible,
+func NewPicker(prompt string, height, width int, r io.Reader) *Picker {
+	candidates := readAllCandidates(r)
+
+	view := NewView(width, height)
+	view.Fill(candidates)
+
+	blank := NewView(width, height)
+	CopyView(blank, view)
+
+	return &Picker{
+		all:    candidates,
+		valid:  len(candidates),
+		prompt: prompt,
+		query:  "",
+		view:   view,
+		blank:  blank,
 	}
-	picker.blank = picker.doAnswer("")
-
-	return picker
 }
+
+func (p *Picker) String() string {
+	return p.prompt + p.query + "\n" + p.view.String()
+}
+
+func (cs CandidateSlice) ToChan() <-chan *Candidate {
+	ch := make(chan *Candidate)
+	go func() {
+		for i := range cs {
+			ch <- &cs[i]
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func (p *Picker) Sort() {
+	if p.query == "" {
+		CopyView(p.view, p.blank)
+		return
+	}
+
+	candidates := p.all[:p.valid]
+
+	ch := make(chan *Candidate)
+	go func() {
+		for i := range candidates {
+			ch <- &candidates[i]
+		}
+		close(ch)
+	}()
+
+	var wg sync.WaitGroup
+	for i := 0; i < 64; i++ {
+		wg.Add(1)
+		go func() {
+			for c := range ch {
+				c.score = Score(c.value, p.query)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	sort.Sort(CandidateSlice(candidates))
+
+	p.valid = sort.Search(len(candidates), func(i int) bool {
+		return candidates[i].score == 0.0
+	})
+
+	p.view.Fill(p.all[:p.valid])
+}
+
+// type View struct {
+// 	Height int
+// 	Query  string
+// 	Rows   []string
+// 	Done   bool
+
+// 	index  int
+// 	prompt string
+// }
+
+// func (v *View) Index() int {
+// 	return v.index
+// }
+
+// func (v *View) Selected() string {
+// 	if len(v.Rows)-1 < v.index {
+// 		return ""
+// 	}
+// 	return v.Rows[v.index]
+// }
+
+// func (v *View) Down() {
+// 	if v.index < len(v.Rows)-1 {
+// 		v.index++
+// 	}
+// }
+
+// func (v *View) Up() {
+// 	if v.index > 0 {
+// 		v.index--
+// 	}
+// }
+
+// func (v *View) ClearPrompt() {
+// 	v.Query = ""
+// 	v.prompt = ""
+// }
+
+// func (v *View) DrawOnTerminal(t *terminal.Terminal) {
+// 	t.HideCursor()
+// 	defer t.ShowCursor()
+
+// 	start_row := t.Height - v.Height - 1
+
+// 	for i, row := range v.toAnsiForm(t) {
+// 		t.WriteToLine(start_row+i, row)
+// 	}
+
+// 	t.MoveTo(start_row, len(v.Query)+len(v.prompt))
+// }
+
+// func (v *View) toAnsiForm(t *terminal.Terminal) []string {
+// 	rows := make([]string, v.Height+1)
+// 	rows[0] = v.prompt + v.Query
+// 	for i, row := range v.Rows {
+// 		rows[i+1] = cutAt(row, t.Width)
+// 	}
+// 	if len(v.Rows) > 0 {
+// 		rows[v.Index()+1] = terminal.AnsiInverted(v.Selected())
+// 	}
+// 	return rows
+// }
+
+// func cutAt(s string, width int) string {
+// 	if len(s) > width {
+// 		return s[:width]
+// 	}
+// 	return s
+// }
+
+// type Picker struct {
+// 	all     []Candidate
+// 	visible int
+// 	blank   *View
+// }
+
+// func NewPicker(candidates []Candidate, visible int) *Picker {
+// 	picker := &Picker{
+// 		all:     candidates,
+// 		visible: visible,
+// 	}
+// 	picker.blank = picker.doAnswer("")
+
+// 	return picker
+// }
 
 type Candidate struct {
 	value string
@@ -105,56 +185,65 @@ func NewCandidate(s string) Candidate {
 	}
 }
 
+func readAllCandidates(r io.Reader) []Candidate {
+	scanner := bufio.NewScanner(r)
+	var candidates []Candidate
+	for scanner.Scan() {
+		candidates = append(candidates, NewCandidate(scanner.Text()))
+	}
+	return candidates
+}
+
 type CandidateSlice []Candidate
 
 func (cs CandidateSlice) Len() int           { return len(cs) }
 func (cs CandidateSlice) Swap(i, j int)      { cs[i], cs[j] = cs[j], cs[i] }
 func (cs CandidateSlice) Less(i, j int) bool { return cs[i].score > cs[j].score }
 
-func (p *Picker) Answer(query string) *View {
-	if query == "" {
-		return p.blank
-	}
-	return p.doAnswer(query)
-}
+// func (p *Picker) Answer(query string) *View {
+// 	if query == "" {
+// 		return p.blank
+// 	}
+// 	return p.doAnswer(query)
+// }
 
-func (p *Picker) doAnswer(query string) *View {
-	all := make(chan *Candidate)
-	go func() {
-		for i := range p.all {
-			all <- &p.all[i]
-		}
-		close(all)
-	}()
+// func (p *Picker) doAnswer(query string) *View {
+// 	all := make(chan *Candidate)
+// 	go func() {
+// 		for i := range p.all {
+// 			all <- &p.all[i]
+// 		}
+// 		close(all)
+// 	}()
 
-	var wg sync.WaitGroup
-	for i := 0; i < 64; i++ {
-		wg.Add(1)
-		go func() {
-			for c := range all {
-				c.score = Score(c.value, query)
-			}
-			wg.Done()
-		}()
-	}
-	wg.Wait()
+// 	var wg sync.WaitGroup
+// 	for i := 0; i < 64; i++ {
+// 		wg.Add(1)
+// 		go func() {
+// 			for c := range all {
+// 				c.score = Score(c.value, query)
+// 			}
+// 			wg.Done()
+// 		}()
+// 	}
+// 	wg.Wait()
 
-	sort.Sort(CandidateSlice(p.all))
+// 	sort.Sort(CandidateSlice(p.all))
 
-	lines := []string{}
-	for i, c := range p.all {
-		if c.score == 0.0 || i >= p.visible {
-			break
-		}
-		lines = append(lines, c.value)
-	}
+// 	lines := []string{}
+// 	for i, c := range p.all {
+// 		if c.score == 0.0 || i >= p.visible {
+// 			break
+// 		}
+// 		lines = append(lines, c.value)
+// 	}
 
-	return &View{
-		Height: p.visible,
-		Rows:   lines,
-		Query:  query,
-		Done:   false,
-		index:  0,
-		prompt: "> ",
-	}
-}
+// 	return &View{
+// 		Height: p.visible,
+// 		Rows:   lines,
+// 		Query:  query,
+// 		Done:   false,
+// 		index:  0,
+// 		prompt: "> ",
+// 	}
+// }
